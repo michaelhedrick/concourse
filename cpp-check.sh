9456e9d14e4cea699f48210069254ee5d9c5ddcc
Michael Hedrick <Michael_Hedrick@comcast.com>
2020-06-08 16:15:46 -0600

    Delete cpp-check.sh

--- cpp-check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o posix
-set -o errexit
-set -o pipefail
-set -ex
-echo "Performing static analysis using cppcheck"
-cppcheck --language=c++ --enable=all -itest --addon=threadsafety.py --xml --error-exitcode=2 ./
